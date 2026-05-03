30c2f2f48514e2c85020d0fe72d31ca1eb80f4ec
huienn04 <iamhuienn04@email.com>
2026-05-03 21:20:53 +0800

    Save my work

--- services/bloom_service.py
+++ services/bloom_service.py
@@ -1,12 +1,32 @@
 def classify_question(text):
+
     text = text.lower()
 
-    remember_keywords = ["define", "list", "identify", "state"]
-    understand_keywords = ["explain", "describe", "summarize", "discuss"]
+    c1_keywords = [
+        "cite", "define", "describe", "extract", "find",
+        "identify", "know", "label", "list", "locate",
+        "match", "measure", "memorise", "name", "organise",
+        "outline", "present", "pronounce", "quote", "recall",
+        "recite", "recognise", "record", "recount", "relate",
+        "reproduce", "select", "state", "tell", "underline",
+        "write"
+    ]
 
-    if any(word in text for word in remember_keywords):
-        return "C1 - Remember"
-    elif any(word in text for word in understand_keywords):
+    c2_keywords = [
+        "account", "alter", "change", "clarify", "classify",
+        "compare", "comprehend", "contrast", "convert", "defend",
+        "depict", "describe", "discover", "discuss", "distinguish",
+        "estimate", "exemplify", "explain", "express", "extend",
+        "find", "formulate", "generalise", "give", "give examples",
+        "illustrate", "indicate", "infer", "interpret", "justify",
+        "locate", "manage", "match"
+    ]
+    for word in c2_keywords:
+      if word in text:
         return "C2 - Understand"
-    else:
-        return "Unknown"
+
+    for word in c1_keywords:
+     if word in text:
+        return "C1 - Remember"
+
+    return "Unknown"
